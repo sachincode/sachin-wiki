b56de9f062f274a337c955b907d3a69e2caf85be
sachincode <shichengzhang@163.com>
2017-03-12 23:31:12 +0800

    add mysql sql

---
+++ sql/mysql.sql
@@ -0,0 +1,51 @@
+use sachin_wiki;
+set names utf8mb4;
+
+CREATE TABLE wiki_article (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `article_title` VARCHAR(100) NOT NULL COMMENT '文章标题',
+  `article_author` VARCHAR(20) NOT NULL COMMENT '文章作者',
+  `parent_id` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT '父文章id',
+  `vm_path` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '静态文件路径',
+  `create_time` TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_article_title` (`article_title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='wiki文章表';
+
+DROP TABLE IF EXISTS wiki_catalog;
+
+CREATE TABLE wiki_catalog (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `wiki_id` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'wiki id',
+  `article_title` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '文章标题',
+  `article_author` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '文章作者',
+  `parent_wiki_id` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT '父文章wiki_id',
+  `create_time` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '创建时间',
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_wiki_id` (`wiki_id`),
+  KEY `idx_article_title` (`article_title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='wiki文章目录表';
+
+DROP TABLE IF EXISTS wiki_content;
+
+CREATE TABLE wiki_content (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `wiki_id` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'wiki id',
+  `content` TEXT COMMENT '文章内容(html)',
+  `create_time` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '创建时间',
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_wiki_id` (`wiki_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='wiki文章内容表';
+
+
+CREATE TABLE wiki_img_src (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `wiki_id` INT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'wiki id',
+  `img_src` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'wiki中img路径',
+  `create_time` DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '创建时间',
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_wiki_id` (`wiki_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='wiki中img路径表';
